24fc21ddc616cef44c98052c6ff8c91fc711d0be
t.kach <t.kach@mail.ru>
2019-07-19 22:49:35 +0300

    Добавлен класс AbstractPart.cs

---
+++ Building-Construction/AbstractPart.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Building_Construction
+{
+    class AbstractPart
+    {
+        public string Name { get; set; }
+        public int Difficulty { get; }
+
+        public AbstractPart(string name)
+        {
+            Name = name;
+            Random rnd = new Random();
+            Difficulty = rnd.Next(1, 10);
+        }
+    }
+}
